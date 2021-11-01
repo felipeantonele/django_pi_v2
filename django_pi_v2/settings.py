5a4792560abcf275420f7ad0239657b7b3297d45
felipeantonele <felipe.antonele@gmail.com>
2021-11-01 20:18:39 -0300

    Configurações para publicar web

--- django_pi_v2/settings.py
+++ django_pi_v2/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',   # add para publicar web
+    'whitenoise.middleware.WhiteNoiseMiddleware',   # add para publicar web
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
