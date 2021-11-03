2849af1899b6e333202416ecb2ec8124a65d2262
felipeantonele <felipe.antonele@gmail.com>
2021-11-03 20:48:29 -0300

    Ajustes

--- core/forms.py
+++ core/forms.py
@@ -5,7 +5,7 @@ from .models import NumbersRegisters, AssociateData, Skills
 
 
 types_of_skills = [
-    ('Habilidade', 'Habilidade'),
+    #('Habilidade', 'Habilidade'),
     ('Hobby', 'Hobby'),
     ('Experiência', 'Experiência'),
     ('Profissão', 'Profissão'),
@@ -22,8 +22,8 @@ class AssociateDataModelForm(forms.ModelForm):
         fields = ['name','responsible_1','responsible_2','phone','email','accept_1','accept_2','accept_3']
     #number_register = forms.CharField(label='Número de Registro', max_length=20)
     name = forms.CharField(label='Nome do Escoteiro', max_length=100)
-    responsible_1 = forms.CharField(label='Nome do Responsável 1', max_length=100)
-    responsible_2 = forms.CharField(label='Nome do Responsável 2', max_length=100)
+    responsible_1 = forms.CharField(label='Nome do Responsável 1', max_length=100, min_length=0)
+    responsible_2 = forms.CharField(label='Nome do Responsável 2', max_length=100, min_length=0)
     phone = forms.CharField(label='Telefone', max_length=15)
     email = forms.EmailField(label='E-mail')
     ac1 = 'Aceita disponibilizar suas informações para a chefia e diretoria do grupo escoteiro?'
